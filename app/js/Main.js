2ce2322edec32a4ac451df5b856fa09002232788
sav-scripts <savorksp@hotmail.com>
2016-07-19 10:13:13 +0800

    added ScalableContent
    ScalableContent help setup viewports and resize contents

--- app/js/Main.js
+++ app/js/Main.js
@@ -5,6 +5,23 @@
 
     _p.init = function()
     {
+        ScalableContent.enableFixFullImage = true;
+        ScalableContent.enableDrawBounds = true;
+
+        $(window).on("resize", onResize);
+        onResize();
     };
+
+    function onResize()
+    {
+        var width = $(window).width(),
+            height = $(window).height();
+
+
+        //ScalableContent.updateView(1280, height);
+
+        ScalableContent.updateView(width, height);
+        ScalableContent.updateResizeAble();
+    }
 
 }());
